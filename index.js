652f5c6da992a867cddbebd6db1cac1c50f504ad
bannanaz <anlepet@gmail.com>
2021-02-25 21:33:38 +0100

    code update

--- index.js
+++ index.js
@@ -1,41 +1,89 @@
-let differenceInDays = require('date-fns/differenceInDays');
 
-/* array of post objects */
-var allPosts = [];
+const list = document.querySelector('.displayList');
+let messageList = [];
 
-/* object constructur */
-function PostObject(name, message) {
-    this.name = name;
-    this.message = message;
-    this.timestamp = new Date();
-};
+document.addEventListener('DOMContentLoaded', initialize);
 
-document.getElementById("submit").addEventListener("click", function () {
-    let name = document.getElementById("name").value;
-    let message = document.getElementById("message").value;   
-    allPosts[allPosts.length] = new PostObject(name, message); // add to the end of allPosts
+function initialize() {
+  getMessages();
+  renderMessageList(messageList);
+}
 
-    //convert array of object into string json and save to local storage
-    localStorage.setItem("myMessages", JSON.stringify(allPosts));
+function addMessage(name, text) {
+  const messageObj = {
+    name,
+    text,
+    id: Date.now(),
+  };
+
+  messageList.push(messageObj);
+  saveMessages();
+  renderMessageList(messageList);
+  console.log(messageList);
+}
+
+const form = document.getElementById('inputForm');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const inputName = document.getElementById('name');
+  const inputText = document.getElementById('message');
+
+  const name = inputName.value;
+  const text = inputText.value;
+
+  if (name || text !== '') {
+    addMessage(name, text);
+  }
 });
 
-//fetch allPosts from local storage, if it exists
-if (localStorage.length !== 0) {
-allPosts = JSON.parse(localStorage.getItem("myMessages")); 
-console.log(allPosts);
+function messageObjToHTML(messageObj) {
+  // givet ett noteObj IN, returnera HTML
+  let LI = document.createElement('li');
+  LI.classList.add("renderedLi");
+
+  LI.innerHTML =
+    `<p>${messageObj.name}</p>
+    <p>${messageObj.text}</p>
+    <p>${messageObj.id}</p>
+  `
+  return LI
 }
 
-  // loop array
-  for (var i in allPosts) {
-      let pastTimestamp = new Date(allPosts[i].timestamp);
-      let nowTimestamp = new Date();
-      let result = differenceInDays(nowTimestamp, pastTimestamp);
-      
-      if (result == 0){
-        document.getElementById("display").innerHTML += "<br>" + (allPosts[i].name + "<br>" + allPosts[i].message + "<br>" + "Idag" + "<br>" + "<br>");
-      }
+function renderMessageList(arr) {
+  list.innerHTML = '';
+  arr.forEach(function (note) {
+    list.appendChild(messageObjToHTML(note));
+  })
+}
 
-      else {
-        document.getElementById("display").innerHTML += "<br>" + (allPosts[i].name + "<br>" + allPosts[i].message + "<br>" + result + " dagar sedan" + "<br>" + "<br>");
+function getMessages() {
+  let MessageStr = localStorage.getItem('allPosts');
+  if (!MessageStr) {
+    return;
+  }
+  messageList = JSON.parse(MessageStr);
+  console.log(messageList);
 }
+
+function saveMessages() {
+  localStorage.setItem('allPosts', JSON.stringify(messageList))
 }
+
+
+
+/*
+// loop array
+for (var i in allPosts) {
+  let pastTimestamp = new Date(allPosts[i].timestamp);
+  let nowTimestamp = new Date();
+  let result = differenceInDays(nowTimestamp, pastTimestamp);
+
+  if (result == 0) {
+    document.getElementById("display").innerHTML += "<br>" + (allPosts[i].name + "<br>" + allPosts[i].message + "<br>" + "Idag" + "<br>" + "<br>");
+  }
+
+  else {
+    document.getElementById("display").innerHTML += "<br>" + (allPosts[i].name + "<br>" + allPosts[i].message + "<br>" + result + " dagar sedan" + "<br>" + "<br>");
+  }
+}
+*/
